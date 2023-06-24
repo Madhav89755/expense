91fd2c6c6a1440a205f0d64e46b52403911c6987
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2023-06-24 15:11:30 +0530

    Update settings.py

--- expense/settings.py
+++ expense/settings.py
@@ -75,26 +75,26 @@ WSGI_APPLICATION = 'expense.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 # DATABASES = {
+    
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'expense',
+#             'USER': 'postgres',
+#             'PASSWORD':'root',
+#             'HOST':'18.208.161.52',
+#             'PORT':'5432'
+#    }
 # }
 
-
-DATABASES = {
-    
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'expense',
-            'USER': 'postgres',
-            'PASSWORD':'root',
-            'HOST':'18.208.161.52',
-            'PORT':'5432'
-   }
-
-}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
