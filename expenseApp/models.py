b48368d3b5785903c1677ba2c003fdea5ef505a5
Madhav89755 <sharma89755@gmail.com>
2024-04-04 23:27:15 +0530

    changes

--- expenseApp/models.py
+++ expenseApp/models.py
@@ -3,26 +3,28 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 mychoices=(
-    ('loan-repayment','loan-repayment'),
-    ('loan-given','loan-given'),
-    ('uncategorized','uncategorized'),
-    ('food','food'),
-    ('travel','travel'),
-    ('salary','salary'),
-    ('income','income'),
-    ('invoice-payment','inovice-payment')
+    ('loan-repayment','Loan-Repayment'),
+    ('loan-given','Loan-Given'),
+    ('uncategorized','Uncategorized'),
+    ('food','Food'),
+    ('travel','Travel'),
+    ('salary','Salary'),
+    ('income','Income'),
+    ('fd','FD'),
+    ('rd','RD'),
+    ('invoice-payment','Inovice-Payment')
 )
 transaction_choice=(
-    ('added','added'),
-    ('debit','debit')
+    ('added','Credit'),
+    ('debit','Debit')
 )
 
 class transaction(models.Model):
     user = models.ForeignKey(to=User ,on_delete=models.CASCADE)
     paid_for = models.CharField(max_length=40, default='Unknown', null=True)
     amount = models.BigIntegerField(null=False, default=0)
-    transaction_type = models.CharField(max_length=20, choices=transaction_choice, null=False)
-    category = models.CharField(max_length=15, default='uncategorized', choices=mychoices)
+    transaction_type = models.CharField(max_length=20, choices=transaction_choice, default='debit', null=False)
+    category = models.CharField(max_length=15, default='food', choices=mychoices)
     paid_on = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.paid_for)
