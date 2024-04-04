58359b8ecc84e7669fc211abee339392f644d5e2
Madhav89755 <sharma89755@gmail.com>
2024-04-04 21:29:39 +0530

    config added

--- expense/settings.py
+++ expense/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$xf&!+r@7ubql^_73d1+l48a&up$x)_93xuvl4=_y7bde_^o58'
+SECRET_KEY = config('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
